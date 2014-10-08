7e3205bcacb358c3c99a767f1f2fade3ad12cc4d
Ionică Bizău <bizauionica@yahoo.com>
2014-10-08 12:36:35 +0300

    Added ConsoleJS

--- public/api/orient.js
+++ public/api/orient.js
@@ -3,5 +3,41 @@
     Z.Starter.orient = function (config, ready) {
         var self = this;
         Z.Starter.orient._api = self.model.people;
+        var $pre = $("pre");
+        ConsoleJs.init({
+            selector: "pre.console",
+            input: "textarea.js-code-to-run"
+        });
+        window._list = function () {
+            Z.Starter.orient._api.req({
+                m: "find"
+            }, function (err, data) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.dir(data);
+                }
+                $pre.stop().animate({
+                    scrollTop: $pre[0].scrollHeight
+                }, 800);
+            });
+        };
+        window._query = function (q) {
+            Z.Starter.orient._api.req({
+                m: "query",
+                q: q
+            }, function (err, data) {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.dir(data);
+                }
+                $pre.stop().animate({
+                    scrollTop: $pre[0].scrollHeight
+                }, 800);
+            });
+        };
+        console.info("Welcome to ConsoleJS!");
+        //_query("insert into people (name, age) values ('Alice', 99)")
     }
 })(Z);
